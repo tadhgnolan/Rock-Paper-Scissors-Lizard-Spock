8aed04dcc532f31d20ca11d5833c7fb7940ea761
Tadhg N <tadhgnolan@gmail.com>
2023-03-10 12:12:51 +0000

    Update transition delay for rules

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,37 +1,39 @@
+/* jshint esversion: 11 */
 const subtitle = document.getElementsByClassName("card-subtitle")[0];
 
 const createWord = (text, index) => {
     const word = document.createElement("span");
+    let delay = Math.floor(Math.random() * 1200);
 
     word.innerHTML = `${text} `;
 
     word.classList.add("card-subtitle-word");
 
-    word.style.transitionDelay = `${index * 40}ms`;
+    word.style.transitionDelay = `${delay}ms`;
 
     return word;
-}
+};
 
 const addWord = (text, index) => subtitle.appendChild(createWord(text, index));
 
 const createSubtitle = text => text.split(" ").map(addWord);
 
-createSubtitle("|| Scissors cuts paper ||");
-
-createSubtitle("Paper covers rock ||");
-
-createSubtitle("Rock crushes lizard ||");
-
-createSubtitle("Lizard poisons Spock ||");
-
-createSubtitle("Spock smashes scissors ||");
-
-createSubtitle("Scissors decapitates lizard ||");
-
-createSubtitle("Lizard eats paper ||");
-
-createSubtitle("Paper disproves Spock ||");
-
-createSubtitle("Spock vaporizes rock ||");
-
-createSubtitle("Rock crushes scissors ||");
+createSubtitle("🔷 Scissors cuts paper");
+subtitle.appendChild(document.createElement("br"));
+createSubtitle("🔷 Paper covers rock");
+subtitle.appendChild(document.createElement("br"));
+createSubtitle("🔷 Rock crushes lizard");
+subtitle.appendChild(document.createElement("br"));
+createSubtitle("🔷 Lizard poisons Spock");
+subtitle.appendChild(document.createElement("br"));
+createSubtitle("🔷 Spock smashes scissors");
+subtitle.appendChild(document.createElement("br"));
+createSubtitle("🔷 Scissors decapitates lizard");
+subtitle.appendChild(document.createElement("br"));
+createSubtitle("🔷 Lizard eats paper");
+subtitle.appendChild(document.createElement("br"));
+createSubtitle("🔷 Paper disproves Spock");
+subtitle.appendChild(document.createElement("br"));
+createSubtitle("🔷 Spock vaporizes rock");
+subtitle.appendChild(document.createElement("br"));
+createSubtitle("🔷 Rock crushes scissors");
